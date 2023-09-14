c15d4d18c8fcb57ce87c76a7c47920be2d0d2635
Zayver <zayber.re@gmail.com>
2023-09-14 12:28:29 -0500

    Initial commit

---
+++ src/main/java/com/ekomodatech/festivanow/event/EventMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ekomodatech.festivanow.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventMicroserviceApplication.class, args);
+	}
+
+}
