ce87c6aa229a6f47aa074a0c30db723e9a1e04a7
David <71355692+sdsuarez@users.noreply.github.com>
2023-10-24 10:14:04 -0500

    Update EventRepository.java

--- src/main/java/com/ekomodatech/festivanow/event/repository/EventRepository.java
+++ src/main/java/com/ekomodatech/festivanow/event/repository/EventRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ekomodatech.festivanow.event.entity.Event;
+
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
     List<Event> findByVisibilityFalse();
